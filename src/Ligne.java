d14dfbe62c77b9bfda76d2f3b33f0485bfdfade6
DeniedPyro <hamdi.jii.99@gmail.com>
2020-03-05 20:02:54 -0500

    ajout de ligne et police

---
+++ src/Ligne.java
@@ -0,0 +1,13 @@
+public class Ligne extends Forme {
+    private int x2;
+    private int y2;
+    public Ligne(x1,y1,x2,y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+    public void dessiner(char[][] matrice) {
+        System.out.println("im a rectangle");
+    }
+}
