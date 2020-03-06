86c8c6a1ab0a5ccdec111d2ed73cb314f551ac08
kaz <fahirah.diarra@gmail.com>
2020-03-06 01:57:25 -0500

    rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -11,7 +11,6 @@ public class Rectangle extends Forme {
     }
 
     public void dessiner(Surface sur) {
-        
         for (int col = this.x ; col < this.largeur + this.x ; col++){
                 sur.setCanevasPoint(this.caractere,this.y, col);
                 sur.setCanevasPoint(this.caractere,(this.hauteur-1)+this.y,col);
