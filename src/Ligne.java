dddd8911bcd76d7f8cc505fbcaf29cbddeb0e47a
DeniedPyro <hamdi.jii.99@gmail.com>
2020-03-05 22:05:23 -0500

    updated ligne.java

--- src/Ligne.java
+++ src/Ligne.java
@@ -1,13 +1,35 @@
+import static java.lang.Math.round;
 public class Ligne extends Forme {
     private int x2;
     private int y2;
-    public Ligne(x1,y1,x2,y2) {
-        this.x1 = x1;
+    public Ligne(int x1, int y1, int x2, int y2, char caractere) {
+        this.x  = x1;
         this.x2 = x2;
-        this.y1 = y1;
+        this.y  = y1;
         this.y2 = y2;
+        this.caractere = caractere;
     }
-    public void dessiner(char[][] matrice) {
-        System.out.println("im a rectangle");
+    public void dessiner(Surface sur) {
+        int deltaX =(x2 - x);
+        int deltaY =(y2 - y);
+        if (deltaX > deltaY) {
+            for(int i = x; i < x2 ; i++) {
+                double calcul = Math.round((deltaY/deltaX)*(i - x) + y);
+                int j = (int) calcul;
+                sur.setCanevasPoint(caractere,i,j);
+            }
+        }
+        else if (deltaX < deltaY) {
+            for(int i = y; i < y2 ; i++) {
+                double calcul = Math.round((deltaX/deltaY)*(i - y) + x);
+                int j = (int)(calcul);
+                sur.setCanevasPoint(caractere,j,i);
+            }
+        }
+        else {
+            for(int i = x; i < x2; i++){
+            sur.setCanevasPoint(caractere,i,i);
+        }
+        }
     }
 }
