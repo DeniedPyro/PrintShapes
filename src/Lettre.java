897823bffa940ccaede89ae18cd7d3a61cd368d6
DeniedPyro <hamdi.jii.99@gmail.com>
2020-03-07 02:49:47 -0500

    Implement Lettre.java and semi working Texte.java

--- src/Lettre.java
+++ src/Lettre.java
@@ -1,16 +1,25 @@
 public class Lettre extends Forme{
-
     char lettre;
-    int x ;
+    int x;
     int y;
-
-    public Lettre (char lettre,int x, int y){
+    public Lettre (char lettre,int x, int y, char caractere){
+        this.caractere = caractere;
         this.lettre = lettre;
         this.x = x;
         this.y = y;
     }
 
     public void dessiner(Surface sur) {
-        System.out.println("im the letter " + this.lettre);
+        byte[] table = Police8x12.getImage(lettre);
+        for(int j = 0; j < 12; j++) {
+            int yActuel = j + this.y;
+            byte encodage = table[j];
+            for(int i = this.x + 7; i >= this.x; i--) {
+                if ((encodage & 1) == 1) {
+                    sur.setCanevasPoint(caractere, yActuel, i);
+                }
+                encodage >>= 1;
+            }
+        }
     }
 }
--- src/Texte.java
+++ src/Texte.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
 public class Texte  extends Forme{
-    Lettre [] liste_lettre ;
+    private ArrayList<Lettre> tabLettres;
 
-    public Texte(char [][] caracteres ,int x , int y){
-        this.liste_lettre = new Lettre [caracteres.length];
+    public Texte(String chaine , int x , int y, char caractere){
+        for(int i = 0; i < chaine.length(); i++){
+            Lettre lettre = new Lettre(chaine.charAt(i), 8*i + x, y, caractere);
+            tabLettres.add(lettre);
+        }
+        this.caractere = caractere;
         this.x = x;
         this.y = y;
-
     }
-
     public void dessiner(Surface sur) {
-
+        for (int i = 0; i < tabLettres.size(); i++){
+            Lettre lettreAct = tabLettres.get(i);
+            lettreAct.dessiner(sur);
+        }
     }
 }
