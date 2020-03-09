aa4795ff0768d989377f75f2e06de655ca9f8d20
DeniedPyro <hamdi.jii.99@gmail.com>
2020-03-08 21:21:57 -0400

    tests

--- src/Cercle.java
+++ src/Cercle.java
@@ -16,7 +16,9 @@ public class Cercle extends Forme {
      * @param y
      */
     private void ajouterPixel(Surface sur, int x , int y){
+        if(x >=0 && x < sur.getColonne() && y >= 0 && y < sur.getLigne()) {
         sur.setPixel(this.caractere, y, x);
+        }
     }
 
     
@@ -26,7 +28,7 @@ public class Cercle extends Forme {
     public void dessiner(Surface sur) {
         int x = 0;
         int y = this.rayon;
-        int m = 5 - 4 * this.rayon;
+        int m = 5 - 4*this.rayon;
 
         while (x <= y){
             ajouterPixel(sur, x + this.x, y + this.y);
@@ -37,13 +39,14 @@ public class Cercle extends Forme {
             ajouterPixel(sur, y + this.x,-x + this.y);
             ajouterPixel(sur,-x + this.x,-y + this.y);
             ajouterPixel(sur,-y + this.x,-x + this.y);
-            if (m > 0)
+            if (m > 0) {
                 y = y - 1;
-                m = m - (8 * y);
+                m = m - (8*y);
             }
             x = x + 1;
-            m = m + 8 * x + 4;
+            m = m + 8*x + 4;
         }
+    }
 
 
     /** Renverse le cercle
